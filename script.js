c5d9578e4d22634d2b6a9ef3eaa8e0c1a0309374
G.Francis <Visions2RealityMG@Gmail.com>
2020-06-16 14:30:17 -0400

    added more comments

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 let screen = document.querySelector('.calculator_screen');  // Target the screen
 let allButtons = document.querySelectorAll('.calculator_buttons'); // Target the buttons 
-let result;
-let currentText = "";
-let prevOperator;
-let foundDecimal = false;
+let result;    // Declared a variable called result but haven't assigned to anything yet
+let currentText = "";  // Declared a variable called currentText assigned to to an empty string
+let prevOperator; // Declared a variable called prevOperator but haven't assigned to anything yet
+let foundDecimal = false; // Declared a variable called foundDecimal thats equal to false 
 
 
-function addAllListeners(param) { 
-    for (let i = 0; i < param.length; i++) {
-        param[i].addEventListener('click', change);
+function addAllListeners(param) {   // Created a function thats takes in a param
+    for (let i = 0; i < param.length; i++) { // iterate through the param length one value at a time
+        param[i].addEventListener('click', change); // Add event listener so when we click on a button we get a change 
     }
 }
-addAllListeners(allButtons);
-function reset() {
-    result = 0;
-    currentText = "";
+addAllListeners(allButtons); 
+function reset() { // created function called reset 
+    result = 0;     //  result is equal to 0
+    currentText = "";  // current text equal to empty string 
 }
-function change(param) {
+function change(param) { // created a function called change which takes in a parameter this function is responsible for a majority of the calculator operations 
     
     if ( param.target.className.match(/operator/)) {
         //fixes multiple decimal points in the same number
@@ -27,28 +27,28 @@ function change(param) {
     
         reset();
     }
-    if (param === undefined || param.target.id == "AC") {
+    if (param === undefined || param.target.id == "AC") { // Resets the screen when AC is pressed 
         //AC was pressed 
         console.log("action AC");
         screen.innerHTML = 0;
         reset();
     } else if (param.target.id == "equals") {
-        //target id is "equals" - compute result
+        //target id is "equals" - when we press the equals button it computes the result
         console.log("action EQUALS");
         if (currentText == "") {
             currentText = 0;
         }
-        result = eval(currentText);
+        result = eval(currentText); // The eval() function evaluates JavaScript code represented as a string. "NEVER USE EVAL"  but for the sake of this project and simplicity I went ahead and used it anyway as a one off.. 
 
-        screen.innerHTML = numberWithCommas(result);
+        screen.innerHTML = numberWithCommas(result); // screen.innerhtml equals numbersWithCommas 
         currentText = result.toString()
         console.log("current text " + currentText);
 
     } else if (param.target.id == "negate") {
         //Replace negate +/ symbol with -
-        val = param.target.innerHTML.replace("\+\/-", " -");
-        currentText = currentText + val;
-        screen.innerHTML = currentText;
+        val = param.target.innerHTML.replace("\+\/-", " -"); // Regular expression
+        currentText = currentText + val; 
+        screen.innerHTML = currentText; 
     } else if (param.target.className.match(/decimal/)) {
         //detect that the decimal was pressed and set flag to prevent it in future
         if (foundDecimal) {
@@ -69,8 +69,8 @@ function change(param) {
     return;
 }
 function numberWithCommas(x) { // Created a function to add commas 
-    var parts = x.toString().split(".");
-    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    return parts.join(".");
+    let parts = x.toString().split("."); // tostring() Converts a number to a string, split() turns a string into an array on the given character
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ","); // replace() replaces the first occurence of a character with another character used a Regular Expression
+    return parts.join("."); // join() converts array into string by concatenating all elements together on a given character
 }
 
